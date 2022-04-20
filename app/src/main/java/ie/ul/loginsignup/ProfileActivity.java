4fd93dd11ad39203ef8fecd930a0c111d5ac23bb
fiston kulimushi <fkulimushi5@gmail.com>
2022-04-21 00:29:45 +0100

    Added a method

--- app/src/main/java/ie/ul/loginsignup/ProfileActivity.java
+++ app/src/main/java/ie/ul/loginsignup/ProfileActivity.java
@@ -31,17 +31,7 @@ public class ProfileActivity extends AppCompatActivity {
     private long pressedTime;
 
 
-    @Override
-    public void onBackPressed() {
-        if (pressedTime + 2000 > System.currentTimeMillis()) {
-            super.onBackPressed();
-            finishAffinity();
-            finish();
-        } else {
-            Toast.makeText(getBaseContext(), "Press back again to exit", Toast.LENGTH_SHORT).show();
-        }
-        pressedTime = System.currentTimeMillis();
-    }
+
 
 
     @Override
